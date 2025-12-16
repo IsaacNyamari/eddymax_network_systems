1f37b3c8d0964630214381e5a1d6cd69eeb51f0d
Isaac Nyamari <jablessions76@gmail.com>
2025-12-16 20:47:15 +0300

    categories with parent updates (1)

---
+++ app/Livewire/CategoryCreate.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class CategoryCreate extends Component
+{
+    use WithFileUploads;
+
+    #[Validate('required|string|min:3|unique:categories,name')]
+    public string $name = '';
+
+    // parent category ID (nullable)
+    #[Validate('nullable|integer|exists:categories,id')]
+    public ?int $parent_category = null;
+
+    #[Validate('nullable|image|mimes:jpg,jpeg,png,webp|max:4096')]
+    public $image;
+
+    public string $slug = '';
+
+    public $categories;
+
+    public function mount()
+    {
+        $this->categories = Category::all();
+    }
+
+    public function saveCategory()
+    {
+        $this->validate();
+
+        $this->slug = Str::slug($this->name);
+
+        $data = [
+            'parent_id' => $this->parent_category ?: null,
+            'name' => $this->name,
+            'slug' => $this->slug,
+        ];
+
+        if ($this->image) {
+            $data['image'] = $this->image->store('products/categories', 'public');
+        }
+
+        Category::create($data);
+
+        // Reset form
+        $this->reset(['name', 'image', 'parent_category']);
+
+        session()->flash('success', 'Category created successfully.');
+    }
+
+    public function render()
+    {
+        return view('livewire.category-create');
+    }
+}
--- resources/views/dashboard/admin/categories/create.blade.php
+++ resources/views/dashboard/admin/categories/create.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
-</div>
+@extends('dashboard.layouts.dashboard')
+@section('title', 'Create Category')
+@section('content')
+    <div class="">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <livewire:category-create />
+            </div>
+        </div>
+    </div>
+@endsection
