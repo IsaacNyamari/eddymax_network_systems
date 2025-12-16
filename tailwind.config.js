b5937820e282551160ba6b1753aac476ef318370
Isaac Nyamari <jablessions76@gmail.com>
2025-12-16 18:17:24 +0300

    added hoverable navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,21 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                maroon: {
+                    50: 'var(--maroon-50)',
+                    100: 'var(--maroon-100)',
+                    200: 'var(--maroon-200)',
+                    300: 'var(--maroon-300)',
+                    400: 'var(--maroon-400)',
+                    500: 'var(--maroon-500)',
+                    600: 'var(--maroon-600)',
+                    700: 'var(--maroon-700)',
+                    800: 'var(--maroon-800)',
+                    900: 'var(--maroon-900)',
+                    950: 'var(--maroon-950)',
+                },
+            },
         },
     },
 
