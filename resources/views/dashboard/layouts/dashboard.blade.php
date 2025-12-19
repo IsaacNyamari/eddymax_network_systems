395ca19dd186f3203a8c1ebf79d9074d6d3e1a5a
Isaac Nyamari <jablessions76@gmail.com>
2025-12-19 13:28:44 +0300

    changes to orders

--- resources/views/dashboard/layouts/dashboard.blade.php
+++ resources/views/dashboard/layouts/dashboard.blade.php
@@ -82,6 +82,15 @@
                                 My Orders
                             </a>
 
+                            <a href="{{ route('customer.orders.index') }}"
+                                class="{{ request()->routeIs('customer.orders.*') ? 'sidebar-active' : '' }} group flex items-center px-2 py-2 text-sm font-medium rounded-md sidebar-hover">
+                                <svg class="mr-3 h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+                                </svg>
+                                Order Returns
+                            </a>
+
                             <a href="{{ route('customer.profile.edit') }}"
                                 class="{{ request()->routeIs('customer.profile.*') ? 'sidebar-active' : '' }} group flex items-center px-2 py-2 text-sm font-medium rounded-md sidebar-hover">
                                 <svg class="mr-3 h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
