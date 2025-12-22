535a811595c1177596ecf9f2b59940062d6856a0
Isaac Nyamari <jablessions76@gmail.com>
2025-12-22 21:09:41 +0300

    update

--- resources/views/livewire/edit-product.blade.php
+++ resources/views/livewire/edit-product.blade.php
@@ -161,19 +161,6 @@
                               </div>
                           @endif
                       @endforeach
-
-                      {{-- <!-- If no categories found -->
-                      @if ($categories-->isEmpty())
-                          <div class="text-center py-8 text-gray-500">
-                              <svg class="w-12 h-12 mx-auto text-gray-300 mb-3" fill="none" stroke="currentColor"
-                                  viewBox="0 0 24 24">
-                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z">
-                                  </path>
-                              </svg>
-                              <p>No categories available</p>
-                          </div>
-                      @endif --}}
                   </div>
 
                   @if ($category_id)
